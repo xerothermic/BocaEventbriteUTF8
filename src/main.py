9cf28b2a0659a594014481e55e8b24984c9d7a4d
Patrick Lu <perf.patrick.lu@gmail.com>
2025-03-28 22:40:25 -0700

    lint
    pylint src

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from tagswa.boca_printer import BocaTcpPrinter, BocaNullPrinter
 
 logger = logging.getLogger(__name__)
 
+
 def configure_root_logger(logging_level=logging.INFO):
     """ Initialize root logger """
     root_logger = logging.getLogger()
@@ -21,6 +22,7 @@ def configure_root_logger(logging_level=logging.INFO):
     root_logger.addHandler(handler)
     root_logger.setLevel(logging_level)
 
+
 def peek(it):
     """ Peek the first element and return the original iterator """
     try:
@@ -29,14 +31,16 @@ def peek(it):
         return None, None
     return first, itertools.chain([first], it)
 
+
 @click.group()
 @click.option('--debug/--no-debug', default=False)
 def cli(debug):
     configure_root_logger(logging.DEBUG if debug else logging.INFO)
 
-@cli.command() # @cli, not @click!
+
+@cli.command()  # @cli, not @click!
 @click.option('--order_id', prompt='Enter your order id',
-                help='Eventbrite Order ID.')
+              help='Eventbrite Order ID.')
 @click.option("--include-used", is_flag=True, default=False, help="Include attendees even if the tickets have been used")
 @click.option('--dry-run', is_flag=True, default=False, help='Dry run only, no printing.')
 @click.option('--first-N', default=0, help='Print first N tickets only.')
@@ -45,7 +49,8 @@ def cli(debug):
 def eventbrite(order_id, include_used, dry_run, first_n, skip_n, ttf_font):
     """ Print Eventbrite tickets """
     ebm = EventbriteManager()
-    attendees = ebm.get_attendees_by_order_id(order_id, unused_only=not include_used)
+    attendees = ebm.get_attendees_by_order_id(
+        order_id, unused_only=not include_used)
     attendee1, attendees = peek(attendees)
     if attendee1 is None:
         logger.error(f"Order {order_id} does not exist.")
@@ -62,26 +67,31 @@ def eventbrite(order_id, include_used, dry_run, first_n, skip_n, ttf_font):
         [next(attendees) for _ in range(skip_n)]
 
     for idx, p in enumerate(attendees):
-        ticket_class_detail = ebm.get_ticket_class_detail(p.event_id, p.ticket_class_id)
+        ticket_class_detail = ebm.get_ticket_class_detail(
+            p.event_id, p.ticket_class_id)
         logger.debug(ticket_class_detail)
         p.ticket_description = ticket_class_detail.description
         if p.event_id == TaiwanAcrobaticTroupeTicket.EVENTID:
-            ticket = TaiwanAcrobaticTroupeTicket(p, common_event_fields, ttf_font)
+            ticket = TaiwanAcrobaticTroupeTicket(
+                p, common_event_fields, ttf_font)
         elif p.event_id == CircusArtTicket.EVENTID:
             ticket = CircusArtTicket(p, common_event_fields, ttf_font)
         elif p.event_id == SummerPicnic2023Ticket.EVENTID:
             if int(p.barcodes[0].barcode) >= 733198138911988989009001:
-                ticket = SummerPicnic2023Ticket(p, common_event_fields, ttf_font)
+                ticket = SummerPicnic2023Ticket(
+                    p, common_event_fields, ttf_font)
             else:
                 continue
         elif p.event_id == WomanTicket.EVENTID:
             ticket = WomanTicket(p, common_event_fields, ttf_font)
         else:
-            raise ValueError(f"Don't know how to create ticket for {p.event_id=}")
+            raise ValueError(
+                f"Don't know how to create ticket for {p.event_id=}")
         # logger.debug(f"{idx=}:{bf}")
         # if first_n == 0 or idx < first_n:
         logger.info(f"Sending {idx} to printer")
         printer.print(ticket.build_boca_script())
+
 
 def _setup_boca_printer(dry_run):
     if dry_run:
@@ -89,6 +99,7 @@ def _setup_boca_printer(dry_run):
     else:
         printer = BocaTcpPrinter()
     return printer
+
 
 if __name__ == '__main__':
     cli()
